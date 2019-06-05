1f4b435101683d6ac44a1953dc9d944ad9e73bb1
Doug Kantor <dkantor1@ford.com>
2019-06-05 07:31:46 -0600

    pcf migration

--- src/main/java/io/pivotal/pal/tracker/TimeEntry.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -60,11 +60,8 @@ public class TimeEntry {
     }
 
     public TimeEntry(long id, long projectId, long userId, LocalDate date, int hours){
+        this(projectId, userId, date, hours);
         this.id = id;
-        this.projectId = projectId;
-        this.userId = userId;
-        this.date = date;
-        this.hours = hours;
     }
 
     public long getId() {
